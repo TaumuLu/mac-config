46a89e577ea9ee0bc9a35e401a97660ea7ddc0cc
TaumuLu <972409545@qq.com>
2020-04-05 16:44:47 +0800

    fix: hammerspoon init.lua

--- configs/.hammerspoon/init.lua
+++ configs/.hammerspoon/init.lua
@@ -3,7 +3,7 @@ require 'plugins.posMouse'
 require 'plugins.yuquePaste'
 
 -- 调试代码
-hs.hotkey.bind({'cmd', 'shift'}, 'h', function()
+hs.hotkey.bind({'cmd', 'option', 'shift'}, 'h', function()
   hs.alert('Hello World from Hammerspoon')
   -- speaker = hs.speech.new()
   -- speaker:speak("Hammerspoon is online")
