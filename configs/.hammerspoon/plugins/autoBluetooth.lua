e3aa6f8c54f1bf4f10c85dbde1881e91807efedb
TaumuLu <972409545@qq.com>
2020-04-11 00:55:02 +0800

    feat: hotkey connect AirPods

--- configs/.hammerspoon/plugins/autoBluetooth.lua
+++ configs/.hammerspoon/plugins/autoBluetooth.lua
@@ -1,7 +1,14 @@
+function getExecBlueutilCmd(params, isExec)
+  local execCmd = "/usr/local/bin/blueutil "
+  local cmd = "[ -x "..execCmd.." ] && "..execCmd..(params)
+  if (isExec ~= nil) then
+    return hs.execute(cmd)
+  end
+  return cmd
+end
+
 function bluetoothSwitch(state)
-  execCmd = "/usr/local/bin/blueutil"
-  cmd = "[ -x "..execCmd.." ] && "..execCmd.." --power "..(state)
-  result = hs.execute(cmd)
+  getExecBlueutilCmd("--power "..(state), true)
 end
 
 function caffeinateCallback(eventType)
@@ -20,3 +27,35 @@ end
 
 caffeinateWatcher = hs.caffeinate.watcher.new(caffeinateCallback)
 caffeinateWatcher:start()
+
+
+
+local name = "Taumu的AirPods"
+
+function trim(text)
+  return string.gsub(text, "[\t\n\r]+", "")
+end
+
+function searchDevice(callback, value)
+  local id = trim(getExecBlueutilCmd("--recent | grep '"..name.."' | awk '{print $2}' | cut -d ',' -f 1", true))
+  if (string.len(id) > 0) then
+    local isConnected = trim(getExecBlueutilCmd("--is-connected "..id, true))
+    if (isConnected == value) then
+      callback(id)
+    end
+  end
+end
+
+local hyper = {'alt'}
+hs.hotkey.bind(hyper, 'l', function()
+  searchDevice(function(id)
+    getExecBlueutilCmd("--connect "..id, true)
+  end, "0")
+end)
+
+local hyper = {'alt', 'shift'}
+hs.hotkey.bind(hyper, 'l', function()
+  searchDevice(function(id)
+    getExecBlueutilCmd("--disconnect "..id, true)
+  end, "1")
+end)
