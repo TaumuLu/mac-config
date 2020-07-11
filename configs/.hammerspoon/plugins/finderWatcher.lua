3be0028b492f57d01d16d216bfa2a039e1b7c7e1
TaumuLu <972409545@qq.com>
2020-07-11 23:18:33 +0800

    fix: finder switch tab hotkey

--- configs/.hammerspoon/plugins/finderWatcher.lua
+++ configs/.hammerspoon/plugins/finderWatcher.lua
@@ -6,22 +6,29 @@ local keyList = {
   hs.keycodes.map.tab
 }
 
-function postKeyEvent(...)
-  local args={...}
-  return function()
-    for i=1, #keyList do
-      local key = keyList[i]
-      local isdown = args[i]
-      if isdown == nil then
-        isdown = true
-      end
-      hs.eventtap.event.newKeyEvent(key, isdown):post(appId)
-    end
-  end
-end
+-- function postKeyEvent(...)
+--   local args={...}
+--   return function()
+--     for i=1, #keyList do
+--       local key = keyList[i]
+--       local isdown = args[i]
+--       if isdown == nil then
+--         isdown = true
+--       end
+--       hs.eventtap.event.newKeyEvent(key, isdown):post(appId)
+--     end
+--   end
+-- end
 
-local switchTabLeft = hs.hotkey.new({'alt', 'cmd'}, 'left', postKeyEvent(true))
-local switchTabRight = hs.hotkey.new({'alt', 'cmd'}, 'right', postKeyEvent(false))
+-- local switchTabLeft = hs.hotkey.new({'alt', 'cmd'}, 'left', postKeyEvent(true))
+-- local switchTabRight = hs.hotkey.new({'alt', 'cmd'}, 'right', postKeyEvent(false))
+
+local switchTabLeft = hs.hotkey.new({'alt', 'cmd'}, 'left', function()
+  hs.eventtap.keyStroke({'cmd','shift'}, '[')
+end)
+local switchTabRight = hs.hotkey.new({'alt', 'cmd'}, 'right', function()
+  hs.eventtap.keyStroke({'cmd','shift'}, ']')
+end)
 
 return {
   id = appId,
