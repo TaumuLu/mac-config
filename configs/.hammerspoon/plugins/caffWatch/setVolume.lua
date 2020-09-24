32d63f84599c3597504e7970b7e8987c81caf1ca
TaumuLu <972409545@qq.com>
2020-09-24 21:30:55 +0800

    fix: set volume get hour bug

--- configs/.hammerspoon/plugins/caffWatch/setVolume.lua
+++ configs/.hammerspoon/plugins/caffWatch/setVolume.lua
@@ -1,15 +1,16 @@
 require 'plugins.caffWatch.connectAirPods'
 
-local date = os.date("*t")
-local hour = date.hour
-local min = date.min
-
 -- hs.alert(os.date("%Y-%m-%d %H:%M:%S", os.time()))
 -- for k,v in pairs(date) do
 --   print(k,v)
 -- end
+-- hs.execute('osascript -e "set volume 10"')
 
 local function triggerVolume()
+  local date = os.date("*t")
+  local hour = date.hour
+  local min = date.min
+
   if (9 <= hour and hour < 19) then
     local id = findDeviceId('connected ')
     if (string.len(id) == 0) then
