e6d52112021735abeb57175f33c982a374afd9b3
TaumuLu <972409545@qq.com>
2020-10-09 15:10:08 +0800

    feat: optimize connect airpods & set volume

--- configs/.hammerspoon/plugins/caffWatch/connectAirPods.lua
+++ configs/.hammerspoon/plugins/caffWatch/connectAirPods.lua
@@ -2,27 +2,27 @@ local function trim(text)
   return string.gsub(text, "[\t\n\r]+", "")
 end
 
-function getExecBlueutilCmd(params, isExec)
+function execBlueutilCmd(params, noExec)
   local execCmd = "/usr/local/bin/blueutil "
   local cmd = "[ -x "..execCmd.." ] && "..execCmd..(params)
-  if (isExec ~= nil) then
-    return trim(hs.execute(cmd))
+  if (noExec ~= nil) then
+    return cmd
   end
-  return cmd
+  return trim(hs.execute(cmd))
 end
 
 function findDeviceId(keyword)
-  return getExecBlueutilCmd("--recent | grep '"..keyword.."' | awk '{print $2}' | cut -d ',' -f 1", true)
+  return execBlueutilCmd("--recent | grep '"..keyword.."' | awk '{print $2}' | cut -d ',' -f 1")
 end
 
 local function bluetoothSwitch(state)
-  getExecBlueutilCmd("--power "..(state), true)
+  execBlueutilCmd("--power "..(state))
 end
 
 local function searchDevice(callback, value, deviceName)
   local id = findDeviceId(deviceName)
   if (string.len(id) > 0) then
-    local isConnected = getExecBlueutilCmd("--is-connected "..id, true)
+    local isConnected = execBlueutilCmd("--is-connected "..id)
     if (isConnected == value) then
       callback(id)
     end
@@ -42,7 +42,10 @@ local function handleDevice(connect)
 
   return function ()
     searchDevice(function(id)
-      getExecBlueutilCmd(param.." "..id, true)
+      if connect == true then
+        hs.execute('osascript -e "set volume output muted 0"')
+      end
+      execBlueutilCmd(param.." "..id)
     end, value, name)
   end
 end
@@ -64,7 +67,7 @@ return {
     bluetoothSwitch(1)
     local timer
     timer = hs.timer.doEvery(1, function ()
-      if getExecBlueutilCmd("--power", true) == "1" then
+      if execBlueutilCmd("--power") == "1" then
         timer:stop()
         connectDevice()
       end
