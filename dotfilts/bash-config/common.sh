45794a7dcc2e8663a873ca4558bb03bb3d8c5058
TaumuLu <taumux@gmail.com>
2024-12-22 22:25:57 +0800

    feat: enhance color output utilities in bash scripts

--- dotfilts/bash-config/common.sh
+++ dotfilts/bash-config/common.sh
@@ -1,37 +1,27 @@
-# 定义颜色映射
-declare -A colors
+#!/bin/bash
 
-colors=(
+# Color output utilities for shell scripts
+# Provides functions for printing colored text to terminal
+
+# ANSI color codes
+declare -A colors=(
     ["red"]='\033[0;31m'
     ["green"]='\033[0;32m'
     ["yellow"]='\033[1;33m'
     ["blue"]='\033[0;34m'
 )
+NC='\033[0m' # No Color
 
-NC='\033[0m'
-
+# Print colored text
+# Usage: printColor "message" "colorName"
 printColor() {
-    local message=$1
-    local colorName=$2
+    local message="$1"
+    local colorName="$2"
     echo -e "${colors[$colorName]}${message}${NC}"
 }
 
-printRed() {
-    local message=$1
-    printColor "$message" "red"
-}
-
-printGreen() {
-    local message=$1
-    printColor "$message" "green"
-}
-
-printYellow() {
-    local message=$1
-    printColor "$message" "yellow"
-}
-
-printBlue() {
-    local message=$1
-    printColor "$message" "blue"
-}
+# Convenience functions for common colors
+printRed() { printColor "$1" "red"; }
+printGreen() { printColor "$1" "green"; }
+printYellow() { printColor "$1" "yellow"; }
+printBlue() { printColor "$1" "blue"; }
