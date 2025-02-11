98b03996660bfe42aa5806f150556b8e905cf5dd
TaumuLu <taumux@gmail.com>
2025-02-11 09:27:20 +0800

    chore: update documentation for configuration settings

--- dotfilts/.finicky.js
+++ dotfilts/.finicky.js
@@ -1,8 +1,10 @@
 // Use https://finicky-kickstart.now.sh to generate basic configuration
 // Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
 
+const defaultBrowser = 'Google Chrome'
+
 module.exports = {
-  defaultBrowser: 'Google Chrome',
+  defaultBrowser,
   options: {
     hideIcon: false,
     checkForUpdate: true,
@@ -16,6 +18,21 @@ module.exports = {
         return opener.bundleId === 'com.larksuite.larkApp'
       },
       browser: 'Google Chrome Canary',
+    },
+    {
+      match: params => {
+        const { url } = params
+        // finicky.log(url.host);
+        return ['smqwe.com'].some(host => url.host.includes(host))
+      },
+      browser: 'Google Chrome Canary',
+    },
+    {
+      match: params => {
+        const { url } = params
+        return ['localhost', '127.0.0.1'].some(host => url.host.includes(host))
+      },
+      browser: defaultBrowser,
     },
   ],
   rewrite: [
