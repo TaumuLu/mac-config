85da41b1856b2125e319f868d6bfc24c43e5b1fa
TaumuLu <taumux@gmail.com>
2025-03-28 23:36:26 +0800

    chore: clean up unused variables in bash scripts

--- dotfilts/.finicky.js
+++ dotfilts/.finicky.js
@@ -1,7 +1,7 @@
 // Use https://finicky-kickstart.now.sh to generate basic configuration
 // Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
 
-const defaultBrowser = 'Google Chrome'
+const defaultBrowser = "Google Chrome";
 
 module.exports = {
   defaultBrowser,
@@ -15,38 +15,42 @@ module.exports = {
       match: ({ opener }) => {
         // finicky.log(opener.bundleId);
         // com.electron.lark
-        return opener.bundleId === 'com.larksuite.larkApp'
+        return opener.bundleId === "com.larksuite.larkApp";
       },
-      browser: 'Google Chrome Canary',
+      browser: "Google Chrome Canary",
     },
     {
-      match: params => {
-        const { url } = params
+      match: (params) => {
+        const { url } = params;
         // finicky.log(url.host);
-        return ['smqwe.com'].some(host => url.host.includes(host))
+        return ["smqwe.com", "larksuite.com"].some((host) =>
+          url.host.includes(host)
+        );
       },
-      browser: 'Google Chrome Canary',
+      browser: "Google Chrome Canary",
     },
     {
-      match: params => {
-        const { url } = params
-        return ['localhost', '127.0.0.1'].some(host => url.host.includes(host))
+      match: (params) => {
+        const { url } = params;
+        return ["localhost", "127.0.0.1"].some((host) =>
+          url.host.includes(host)
+        );
       },
       browser: defaultBrowser,
     },
   ],
   rewrite: [
     {
-      match: ({ url }) => url.host.endsWith('zentao2.siyecao1.com'),
-      url: params => {
+      match: ({ url }) => url.host.endsWith("zentao2.siyecao1.com"),
+      url: (params) => {
         // finicky.log(JSON.stringify(params));
-        const { url } = params
+        const { url } = params;
         return {
           ...url,
-          host: 'zentao2.smqwe.com',
-          protocol: 'http',
-        }
+          host: "zentao2.smqwe.com",
+          protocol: "http",
+        };
       },
     },
   ],
-}
+};
